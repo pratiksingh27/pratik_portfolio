8d1a27b9a6892101dc0cf4ad9e43d27360602b84
pratiksingh27 <pratik27march@gmail.com>
2024-04-08 22:08:50 +0530

    fixing react-no-unescaped

--- src/pages/about.js
+++ src/pages/about.js
@@ -54,7 +54,7 @@ const about = () => {
                 Know me More
               </h2>
               <p className="font-medium">
-                Hi, I'm Pratik, a web developer and UI/UX designer with a
+                Hi, I&apos;m Pratik, a web developer and UI/UX designer with a
                 passion for creating beautiful, functional, and user-centered
                 digital experiences. With 2+ years of experience in the field. I
                 am always looking for new and innovative ways to bring my
@@ -63,12 +63,12 @@ const about = () => {
 
               <p className="font-medium my-4">
                 I believe that design is about more than just making things look
-                pretty , it's about solving problems and creating intuitive,
+                pretty , it&apos;s about solving problems and creating intuitive,
                 enjoyable experiences for users.
               </p>
 
               <p className="font-medium">
-                Whether I'm working on a website, mobile app, or other digital
+                Whether I&apos;m working on a website, mobile app, or other digital
                 product, I bring my commitment to design excellence and
                 user-centered thinking to every project I work on. I look
                 forward to the opportunity to bring my skills and passion to
